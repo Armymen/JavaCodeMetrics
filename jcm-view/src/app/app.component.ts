05dbf54c2164a97ce4a3e72e0cc513c17127b76e
p56f <pawelcc@o2.pl>
2017-03-17 11:52:11 +0100

    Angular project - greetings

--- jcm-view/src/app/app.component.ts
+++ jcm-view/src/app/app.component.ts
@@ -1,7 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Greeting } from './greeting';
+import { GreetingsService } from './greetings.service';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`
+  template: `<h1>Hello {{greeting.name}}. Your id is {{greeting.id}}.</h1>`,
+  providers: [GreetingsService]
 })
-export class AppComponent { name = 'Angular'; }
+
+export class AppComponent implements OnInit { 
+  name = 'Pawel';
+  greeting: Greeting;
+
+  constructor(private greetingsService: GreetingsService) {}
+
+  ngOnInit(): void {
+    this.setGreeting();
+  }
+
+  setGreeting(): void {
+    this.greetingsService.getGreeting(name).then(greeting => this.greeting = greeting);
+  }
+}
---
+++ jcm-view/src/app/greetings.service.ts
@@ -0,0 +1,28 @@
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Greeting } from './greeting';
+
+@Injectable()
+export class GreetingsService {
+
+    private headers = new Headers({'Content-Type': 'application/json'});
+    private greetingsUrl = 'http://localhost:12800';
+
+    constructor(private http: Http) {}
+
+    getGreeting(name: string): Promise<Greeting> {
+        const url = `${this.greetingsUrl}/greeting?name=${name}`;
+        return this.http.get(this.greetingsUrl)
+               .toPromise()
+               .then(response => response.json().data as Greeting)
+               .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    }   
+}
