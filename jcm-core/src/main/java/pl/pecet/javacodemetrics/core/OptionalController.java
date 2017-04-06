eb1e13bce4e11929e36e6caf958a10d58ad5710d
p56f <pawelcc@o2.pl>
2017-04-06 09:07:07 +0200

    Add optional controller

---
+++ jcm-core/src/main/java/pl/pecet/javacodemetrics/core/OptionalController.java
@@ -0,0 +1,18 @@
+package pl.pecet.javacodemetrics.core;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OptionalController {
+
+	@GetMapping("/optionals")
+	public String getOptionals() {
+		Optional<String> o1 = Optional.of("Hello, ");
+		Optional<String> o2 = Optional.of("World!");
+		
+		return o1.flatMap(first -> o2.flatMap(sec -> Optional.of(first + sec))).get();
+	}
+}
