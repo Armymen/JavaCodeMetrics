ef4f8e52e1651acdbbee0dd68cc6af68fc9ad267
p56f <pawelcc@o2.pl>
2017-04-15 19:07:55 +0200

    Set name of MongoDB database and tables

--- jcm-core/src/main/java/pl/pecet/javacodemetrics/core/domain/Project.java
+++ jcm-core/src/main/java/pl/pecet/javacodemetrics/core/domain/Project.java
@@ -1,15 +1,16 @@
 package pl.pecet.javacodemetrics.core.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
 @Data
+@Document(collection = "projects")
 public class Project {
 
 	@Id
 	private String id;
 
 	private final String name;
-
 }
