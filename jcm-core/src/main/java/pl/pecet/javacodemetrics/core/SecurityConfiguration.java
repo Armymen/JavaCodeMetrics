7da64d365a7c6865be4462310b3085f7843c112c
p56f <pawelcc@o2.pl>
2017-04-16 21:02:31 +0200

    Add security configuration

---
+++ jcm-core/src/main/java/pl/pecet/javacodemetrics/core/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package pl.pecet.javacodemetrics.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/").permitAll().anyRequest()
+				.authenticated();
+	}
+
+	@Autowired
+	public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("passwd").roles("ADMIN, USER");
+	}
+}
