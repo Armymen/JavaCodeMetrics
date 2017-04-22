520e4c5bf37b2fb8e8beb9d42bd9592ac1278a63
p56f <pawelcc@o2.pl>
2017-04-22 13:09:13 +0200

    Changes in security config, introduce YAML instead of properties, preparing support for JWT

--- jcm-core/src/main/java/pl/pecet/javacodemetrics/core/SecurityConfiguration.java
+++ jcm-core/src/main/java/pl/pecet/javacodemetrics/core/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsUtils;
 
 import lombok.AllArgsConstructor;
@@ -20,9 +19,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
-		http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/").permitAll()
-				.requestMatchers(CorsUtils::isPreFlightRequest).permitAll().anyRequest().authenticated().and().csrf()
-				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		http.httpBasic().and().authorizeRequests().requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+				.anyRequest().authenticated().and().csrf().disable();
 	}
 
 	@Override
