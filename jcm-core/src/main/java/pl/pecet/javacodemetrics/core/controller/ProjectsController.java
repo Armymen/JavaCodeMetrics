25f060db32759949f3af0a2254c2a8510ecb0cbe
p56f <pawelcc@o2.pl>
2017-04-29 14:30:30 +0200

    Extension of adding new project functionality

--- jcm-core/src/main/java/pl/pecet/javacodemetrics/core/controller/ProjectsController.java
+++ jcm-core/src/main/java/pl/pecet/javacodemetrics/core/controller/ProjectsController.java
@@ -2,20 +2,32 @@ package pl.pecet.javacodemetrics.core.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
 import pl.pecet.javacodemetrics.core.domain.Project;
+import pl.pecet.javacodemetrics.core.security.auth.JwtTokenUtil;
 import pl.pecet.javacodemetrics.core.service.ProjectsService;
 
 @RestController
-@AllArgsConstructor
 public class ProjectsController {
 
+	@Value("${jwt.header}")
+	private String tokenHeader;
+
 	private final ProjectsService projectsService;
+
+	private final JwtTokenUtil jwtTokenUtil;
+
+	public ProjectsController(final ProjectsService projectsService, final JwtTokenUtil jwtTokenUtil) {
+		this.projectsService = projectsService;
+		this.jwtTokenUtil = jwtTokenUtil;
+	}
 
 	@GetMapping("projects")
 	public List<Project> getAllProjects() {
@@ -28,7 +40,9 @@ public class ProjectsController {
 	}
 
 	@PostMapping("addNewProject/{name}")
-	public Project addNewProject(@PathVariable final String name) {
-		return projectsService.addNewProject(name);
+	public Project addNewProject(final HttpServletRequest request, @PathVariable final String name) {
+		final String token = request.getHeader(tokenHeader);
+		final String username = jwtTokenUtil.getUsernameFromToken(token);
+		return projectsService.addNewProject(username, name);
 	}
 }
--- jcm-core/src/main/java/pl/pecet/javacodemetrics/core/service/ProjectsService.java
+++ jcm-core/src/main/java/pl/pecet/javacodemetrics/core/service/ProjectsService.java
@@ -1,13 +1,16 @@
 package pl.pecet.javacodemetrics.core.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
+import pl.pecet.javacodemetrics.core.domain.JcmUser;
 import pl.pecet.javacodemetrics.core.domain.Project;
 import pl.pecet.javacodemetrics.core.repository.ProjectsRepository;
+import pl.pecet.javacodemetrics.core.repository.UserRepository;
 
 @Service
 @Transactional
@@ -16,7 +19,17 @@ public class ProjectsService {
 
 	private final ProjectsRepository projectsRepository;
 
-	public Project addNewProject(final String name) {
+	private final UserRepository userRepository;
+
+	public Project addNewProject(final String username, final String name) {
+		final JcmUser user = userRepository.findOneByName(username);
+
+		if (user.getProjects() == null) {
+			user.setProjects(new ArrayList<>());
+		}
+		user.getProjects().add(name);
+		userRepository.save(user);
+
 		return projectsRepository.save(new Project(name));
 	}
 
