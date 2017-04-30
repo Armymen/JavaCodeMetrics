88ae6e04cfa45aaf3d44d7587f7474f38b8c3369
p56f <pawelcc@o2.pl>
2017-04-30 09:41:21 +0200

    Read current user from database

--- jcm-view/src/app/_services/user.service.ts
+++ jcm-view/src/app/_services/user.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 
+import { AuthenticationService } from './index';
+
 @Injectable()
 export class UserService {
-    constructor(private http: Http) {}
+    constructor(private http: Http, private authenticationService: AuthenticationService) {}
 
-    private createUserUrl = 'http://localhost:12800/createUser';
-    private headers = new Headers({'Content-Type': 'application/json'});
+    private baseUrl = 'http://localhost:12800';
+
+    getCurrentUser() {
+        let authHeaders = new Headers({'Authorization': this.authenticationService.getToken()});
+        return this.http.get(this.baseUrl + '/user', {headers: authHeaders})
+            .map((response: Response) => response.json());
+    }
 
     create(username: string, password: string) {
-        return this.http.post(this.createUserUrl, JSON.stringify({username: username, password: password}), {headers: this.headers})
+        let jsonHeaders = new Headers({'Content-Type': 'application/json'});
+        return this.http.post(this.baseUrl + '/createUrl', JSON.stringify({username: username, password: password}), {headers: jsonHeaders})
             .map((response: Response) => response.json());
     }
 }
--- jcm-view/src/app/user_profile/user.profile.component.ts
+++ jcm-view/src/app/user_profile/user.profile.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { User } from '../_models/index';
+import { UserService } from '../_services/index';
 
 @Component({
     moduleId: module.id,
     templateUrl: '../../../app/user_profile/user.profile.component.html'
 })
 
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
     currentUser: User;
 
-    constructor() {
-        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    constructor(private userService: UserService) {}
+
+    ngOnInit() {
+        this.userService.getCurrentUser().subscribe(currentUser => this.currentUser = currentUser);
     }
 }
