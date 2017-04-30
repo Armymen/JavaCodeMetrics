85b73caaf6fd676be1db51c8e48a0c3844a12b9e
p56f <pawelcc@o2.pl>
2017-04-30 09:18:32 +0200

    Changes in authentication and user controllers

--- jcm-core/src/main/java/pl/pecet/javacodemetrics/core/controller/AuthenticationController.java
+++ jcm-core/src/main/java/pl/pecet/javacodemetrics/core/controller/AuthenticationController.java
@@ -1,8 +1,5 @@
 package pl.pecet.javacodemetrics.core.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,34 +8,24 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import pl.pecet.javacodemetrics.core.security.auth.JwtAuthenticationRequest;
 import pl.pecet.javacodemetrics.core.security.auth.JwtAuthenticationResponse;
 import pl.pecet.javacodemetrics.core.security.auth.JwtTokenUtil;
-import pl.pecet.javacodemetrics.core.service.UserDetailsImpl;
 
 @RestController
+@AllArgsConstructor
 public class AuthenticationController {
-
-	@Value("${jwt.header}")
-	private String tokenHeader;
 
 	private final AuthenticationManager authenticationManager;
 
 	private final JwtTokenUtil jwtTokenUtil;
 
 	private final UserDetailsService userDetailsService;
-
-	public AuthenticationController(final AuthenticationManager authenticationManager, final JwtTokenUtil jwtTokenUtil,
-			final UserDetailsService userDetailsService) {
-		this.authenticationManager = authenticationManager;
-		this.jwtTokenUtil = jwtTokenUtil;
-		this.userDetailsService = userDetailsService;
-	}
 
 	@PostMapping("${jwt.route.authentication.path}")
 	public ResponseEntity<?> createAuthenticationToken(
@@ -47,18 +34,9 @@ public class AuthenticationController {
 				.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
 						authenticationRequest.getPassword()));
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-		final UserDetailsImpl userDetails = (UserDetailsImpl) userDetailsService
-				.loadUserByUsername(authenticationRequest.getUsername());
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
 		final String token = jwtTokenUtil.generateToken(userDetails);
 
-		return ResponseEntity
-				.ok(new JwtAuthenticationResponse(userDetails.getUsername(), token, userDetails.getProjects()));
-	}
-
-	@GetMapping("user")
-	public UserDetails getAuthenticatedUser(final HttpServletRequest request) {
-		final String token = request.getHeader(tokenHeader);
-		final String username = jwtTokenUtil.getUsernameFromToken(token);
-		return userDetailsService.loadUserByUsername(username);
+		return ResponseEntity.ok(new JwtAuthenticationResponse(token));
 	}
 }
--- jcm-core/src/main/java/pl/pecet/javacodemetrics/core/controller/UserController.java
+++ jcm-core/src/main/java/pl/pecet/javacodemetrics/core/controller/UserController.java
@@ -1,19 +1,45 @@
 package pl.pecet.javacodemetrics.core.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
 import pl.pecet.javacodemetrics.core.domain.JcmUser;
 import pl.pecet.javacodemetrics.core.security.auth.JwtAuthenticationRequest;
+import pl.pecet.javacodemetrics.core.security.auth.JwtTokenUtil;
 import pl.pecet.javacodemetrics.core.service.UserService;
 
 @RestController
-@AllArgsConstructor
 public class UserController {
 
+	@Value("${jwt.header}")
+	private String tokenHeader;
+
 	private final UserService userService;
+
+	private final UserDetailsService userDetailsService;
+
+	private final JwtTokenUtil jwtTokenUtil;
+
+	public UserController(final UserService userService, final UserDetailsService userDetailsService,
+			final JwtTokenUtil jwtTokenUtil) {
+		this.userService = userService;
+		this.userDetailsService = userDetailsService;
+		this.jwtTokenUtil = jwtTokenUtil;
+	}
+
+	@GetMapping("user")
+	public UserDetails getCurrentUser(final HttpServletRequest request) {
+		final String token = request.getHeader(tokenHeader);
+		final String username = jwtTokenUtil.getUsernameFromToken(token);
+		return userDetailsService.loadUserByUsername(username);
+	}
 
 	@PostMapping("createUser")
 	public JcmUser create(@RequestBody final JwtAuthenticationRequest userCredentials) {
