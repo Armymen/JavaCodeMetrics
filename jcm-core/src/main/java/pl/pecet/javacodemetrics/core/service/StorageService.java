09d59143c33a0e054cb5fd8ca66b1c7f55f6294c
p56f <pawelcc@o2.pl>
2017-05-11 17:58:48 +0200

    Extends storage service

--- jcm-core/src/main/java/pl/pecet/javacodemetrics/core/service/StorageService.java
+++ jcm-core/src/main/java/pl/pecet/javacodemetrics/core/service/StorageService.java
@@ -4,6 +4,8 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +20,7 @@ public class StorageService {
 
 	public String store(final String name, final MultipartFile file) throws IOException {
 		final InputStream fileAsStream = new ByteArrayInputStream(file.getBytes());
+		gridFsTemplate.delete(new Query(Criteria.where("filename").is(name)));
 		return gridFsTemplate.store(fileAsStream, name).getId().toString();
 	}
 }
