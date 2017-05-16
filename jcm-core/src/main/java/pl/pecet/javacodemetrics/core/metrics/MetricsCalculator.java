6a04f82be04d65545fec9c0eac3cc4f2744cd30c
p56f <pawelcc@o2.pl>
2017-05-16 19:25:13 +0200

    Introduction example code of AST parsing

--- jcm-core/src/main/java/pl/pecet/javacodemetrics/core/metrics/MetricsCalculator.java
+++ jcm-core/src/main/java/pl/pecet/javacodemetrics/core/metrics/MetricsCalculator.java
@@ -1,5 +1,51 @@
 package pl.pecet.javacodemetrics.core.metrics;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class MetricsCalculator {
 
+	private final InputStream inputStream;
+
+	public void calculate() {
+		try (final ZipInputStream zis = new ZipInputStream(inputStream)) {
+			ZipEntry entry;
+			while ((entry = zis.getNextEntry()) != null) {
+				// TODO handle calculate metrics for single entry
+			}
+		} catch (final IOException e) {
+			// TODO exception handling
+		}
+	}
+
+	public List<String> findMethodNames() {
+		final List<String> methodNames = new ArrayList<>();
+
+		final ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setSource("public class HelloClass { public static void main(String... args) {} void someMethod(){}}"
+				.toCharArray());
+		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+		cu.accept(new ASTVisitor() {
+			@Override
+			public boolean visit(final MethodDeclaration methodDeclaration) {
+				methodNames.add(methodDeclaration.getName().getFullyQualifiedName());
+				return super.visit(methodDeclaration);
+			}
+		});
+
+		return methodNames;
+	}
 }
--- jcm-core/src/test/java/pl/pecet/javacodemetrics/core/metrics/MetricsCalculatorTest.java
+++ jcm-core/src/test/java/pl/pecet/javacodemetrics/core/metrics/MetricsCalculatorTest.java
@@ -1,14 +1,31 @@
 package pl.pecet.javacodemetrics.core.metrics;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class MetricsCalculatorTest {
 
+	private MetricsCalculator tested;
+
+	@Before
+	public void setUp() {
+		tested = new MetricsCalculator(null);
+	}
+
 	@Test
-	public void test() {
-		assertTrue(true);
+	public void shouldFindMethodNames() {
+		final List<String> methodNames = tested.findMethodNames();
+
+		assertEquals(2, methodNames.size());
+		assertTrue(methodNames.contains("main"));
+		assertTrue(methodNames.contains("someMethod"));
+		assertFalse(methodNames.contains("abc"));
 	}
 
 }
